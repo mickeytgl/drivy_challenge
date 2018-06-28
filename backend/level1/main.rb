9de978c00a889a1c97c14a305979803b3e010ff2
Miguel Torres <migueltg93@gmail.com>
2018-06-27 19:08:45 -0500

    Refactor

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,41 +1,33 @@
 require 'json'
 require 'date'
 
-$file = File.read('data/input.json')
-$data = JSON.parse($file)
+file = File.read('data/input.json')
+data = JSON.parse(file)
 final = { "rentals" => [] }
-@rentals = $data["rentals"]
+@rentals = data["rentals"]
+@cars = data["cars"]
 
 def day_difference(i)
-  end_date = Date.parse($data["rentals"][i]["end_date"])
-  start_date = Date.parse($data["rentals"][i]["start_date"])
+  end_date = Date.parse(@rentals[i]["end_date"])
+  start_date = Date.parse(@rentals[i]["start_date"])
 
   (end_date - start_date).to_i
 end
 
-def car_id(i)
-  $data["rentals"][i]["car_id"]
-end
-
-def distance(i)
-  $data["rentals"][i]["distance"]
-end
-
 def price_per_km_and_per_day(i)
-  car = $data["cars"].find{ |car| car["id"] == car_id(i)  }
-  price_per_km, price_per_day = car["price_per_km"], car["price_per_day"]
+  rented_car = @cars.find{ |car| car["id"] == @rentals[i]["car_id"]  }
+  price_per_km, price_per_day = rented_car["price_per_km"], car["price_per_day"]
 end
 
-@rentals.each_with_index do |rental, i|
+def calculate_price(i)
   ppkm = price_per_km_and_per_day(i)[0]
   ppd = price_per_km_and_per_day(i)[1]
 
-  result = day_difference(i) * ppd + ppkm * distance(i)
-  result_hash = { "id": rental["id"], "price": result }
-  final["rentals"] << result_hash
-  puts final
+  result = day_difference(i) * ppd + ppkm * @rentals[i]["distance"]
 end
 
-class ClassName
-
+@rentals.each_with_index do |rental, i|
+  result = { "id": rental["id"], "price": calculate_price(i) }
+  final["rentals"] << result
+  puts final
 end
